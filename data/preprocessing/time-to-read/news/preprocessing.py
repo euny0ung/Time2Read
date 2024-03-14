bfe36e9b16e1fcae3fc614e857317abe884303fb
이민우 <lmw7414@naver.com>
2024-03-14 16:17:05 +0900

    #7 - feat : [Data] email 제거 함수 추가

--- data/preprocessing/time-to-read/news/preprocessing.py
+++ data/preprocessing/time-to-read/news/preprocessing.py
@@ -8,3 +8,12 @@ def delete_html_tag(context):
         if text:
             processed_context.append(text)
     return processed_context
+
+# email 지우기
+def delete_email(context):
+    preprocessed_text = []
+    for text in context:
+        text = re.sub(r'[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', '', text).strip()
+        if text:
+            preprocessed_text.append(text)
+    return preprocessed_text
