8a6b2198a015a702d324827349a89c26737e4d4a
이민우 <lmw7414@naver.com>
2024-03-14 16:20:02 +0900

    #7 - feat : [Data] 특수문자 관련 제거 함수 추가

--- data/preprocessing/time-to-read/news/preprocessing.py
+++ data/preprocessing/time-to-read/news/preprocessing.py
@@ -25,3 +25,44 @@ def delete_hashtag(context):
         text = re.sub(r'#\S+', '', text).strip()
         if text:
             preprocessed_text.append(text)
+
+# 저작권 관련 텍스트 지우기
+def delete_copyright(context):
+    re_patterns = [
+        r'\<저작권자(\(c\)|©|(C)|(\(C\))).+?\>',
+        r'(Copyrights)|(\(c\))|(\(C\))|©|(C)|'
+    ]
+    preprocessed_text = []
+    for text in context:
+        for re_pattern in re_patterns:
+            text = re.sub(re_pattern, '', text).strip()
+        if text:
+            preprocessed_text.append(text)
+    return preprocessed_text
+
+# 특수문자 지우기
+def delete_special_symbol(context):
+    preprocessed_text = []
+    for text in context:
+        text = re.sub(r'□|■|▲|&[a-zA-Z]+;', '', text)
+        if text:
+            preprocessed_text.append(text)
+    return preprocessed_text
+
+# [%%IMAGE%%] 지우기
+def delete_imgtag(context):
+    preprocessed_text = []
+    for text in context:
+        text = re.sub(r'\[%%.*?%%\]', '', text).strip()
+        if text:
+            preprocessed_text.append(text)
+    return preprocessed_text
+
+# 특수문자로 시작하는 불필요한 문장 지우기
+def delete_start_with_special_symbols(context):
+    preprocessed_text = []
+    for text  in context:
+        text = re.sub(r'(▶|☞)[^\n]*\n?', '', text).strip()
+        if text:
+            preprocessed_text.append(text)
+    return preprocessed_text
