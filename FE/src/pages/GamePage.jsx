99f5842db2cf8bb062b8a15ff734430779f3c5bf
euny0ung <hdtv0218@gmail.com>
2024-03-18 09:40:44 +0900

    #21 - feat : 퀴즈 mock API 적용 및 상태 저장

--- FE/src/pages/GamePage.jsx
+++ FE/src/pages/GamePage.jsx
@@ -5,14 +5,20 @@ import Player from '@components/game/Player';
 import { OrbitControls, PointerLockControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
+<<<<<<< HEAD
 import { useQuizStore } from '@stores/store';
+=======
+>>>>>>> d6b57f9 (#20 - feat: 미로 텍스쳐링, 랜딩 페이지 기본 설정)
 
 const GamePage = () => {
   const [isPlayerMode, setIsPlayerMode] = useState(true); // 1인칭, 3인칭 모드 전환. 테스트할 때 편하라고 만듦
   const { quiz } = useQuizStore();
 
+<<<<<<< HEAD
   console.log(quiz);
 
+=======
+>>>>>>> d6b57f9 (#20 - feat: 미로 텍스쳐링, 랜딩 페이지 기본 설정)
   return (
     <>
       <div className="w-screen h-screen overflow-hidden">
@@ -37,7 +43,11 @@ const GamePage = () => {
         {/* 버튼 클릭으로 컨트롤 모드 전환 */}
         <button
           className="absolute top-2.5 left-2.5 bg-gray-500 text-white font-bold py-2 px-4 rounded hover:bg-gray-700 focus:outline-none focus:shadow-outline"
+<<<<<<< HEAD
           onClick={() => setIsPlayerMode(!isPlayerMode)}
+=======
+          onClick={() => setIsPlayerMode(!isOrbit)}
+>>>>>>> d6b57f9 (#20 - feat: 미로 텍스쳐링, 랜딩 페이지 기본 설정)
         >
           {isPlayerMode ? '3인칭 모드로 전환' : '1인칭 모드로 전환'}
         </button>
