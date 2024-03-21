00411e15f0da69f9864598873e2fd111aa1a908f
shinyeji28 <xntm10@naver.com>
2024-03-21 11:15:31 +0900

    #27 - refector : [BE] kakao 요청코드 service로 옮김, exception 처리

--- back-end/src/main/java/org/ssafy/bibibig/oauth/api/SocialController.java
+++ back-end/src/main/java/org/ssafy/bibibig/oauth/api/SocialController.java
@@ -3,18 +3,13 @@ package org.ssafy.bibibig.oauth.api;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.ssafy.bibibig.common.dto.ErrorCode;
 import org.ssafy.bibibig.common.dto.Response;
 import org.ssafy.bibibig.oauth.dto.MemberInfo;
 import org.ssafy.bibibig.oauth.dto.response.LoginResponse;
-import org.ssafy.bibibig.oauth.dto.response.TokenResponse;
-import org.ssafy.bibibig.oauth.dto.response.UserResponse;
+import org.ssafy.bibibig.oauth.service.KakaoService;
 import org.ssafy.bibibig.oauth.service.SocialService;
 
 import java.util.Map;
@@ -24,94 +19,28 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class SocialController {
 
-    @Value("${oauth.KAKAO_CLIENT_ID}") String CLIENT_ID;
-    @Value("${oauth.KAKAO_AUTHORIZE_URL}") String AUTHORIZE_URL;
-    @Value("${oauth.KAKAO_REDIRECT_URL}") String REDIRECT_URL;
-    @Value("${oauth.KAKAO_TOKEN_REQUEST_URL}") String TOKEN_REQUEST_URL;
-    @Value("${oauth.KAKAO_USER_INFO_REQUEST_URL}") String USER_INFO_REQUEST_URL;
-    @Value("${oauth.KAKAO_SECURE_RESOURCE}") String SECURE_RESOURCE;
-
     private final SocialService socialService;
-
-//    @GetMapping("/kakao")
-//    public ResponseEntity<?> requestLogin(){
-//        // kakao로 login요청
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        return restTemplate.exchange(
-//               AUTHORIZE_URL+"?response_type=code&client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URL,
-//                HttpMethod.GET,
-//                null,
-//                String.class
-//        );
-//    }
+    private final KakaoService kakaoService;
 
     @GetMapping("/kakao/login")
     public ResponseEntity<?> getToken(@RequestParam String code){
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("grant_type", "authorization_code");
-        body.add("client_id", CLIENT_ID);
-        body.add("redirect_uri", REDIRECT_URL);
-        body.add("code", code);
-
-        // 요청 엔티티 생성
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
-
-        // 요청 보내기
-        return restTemplate.exchange(
-                TOKEN_REQUEST_URL,
-                HttpMethod.POST,
-                requestEntity,
-                TokenResponse.class
-        );
-
-
+        return kakaoService.requestToken(code);
     }
 
     @PostMapping("/kakao/account")
     private ResponseEntity<?> checkAuthentication(@RequestBody Map<String, String> requestBody, HttpServletRequest request){
         String token = requestBody.get("token");
 
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setBearerAuth(token);
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("secure_resource", SECURE_RESOURCE);
-        body.add("property_keys", "[\"kakao_account.name\", \"kakao_account.email\"]");
-
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
-
-        ResponseEntity<UserResponse> responseEntity = restTemplate.exchange(
-                USER_INFO_REQUEST_URL,
-                HttpMethod.GET,
-                requestEntity,
-                UserResponse.class
-        );
-        UserResponse userResponse = responseEntity.getBody();
-        if(userResponse==null){
-            // token 이상
-            return ResponseEntity.ok("토큰 만료");
+        MemberInfo memberInfo = kakaoService.requestAccount(token);
+        if(memberInfo==null){
+            return Response.error(ErrorCode.INVALID_TOKEN);
         }
 
-        MemberInfo memberInfo = MemberInfo.of(null,userResponse.getKakaoAccount().getName(), userResponse.getKakaoAccount().getEmail());
-
-        // DB에 회원이 없으면 회원가입 처리
         memberInfo = socialService.checkLogin(memberInfo);
-
-        // 세션 발급
         generateSession(memberInfo, request);
 
         LoginResponse loginResponse = LoginResponse.of(memberInfo.getName());
+
         return Response.success(loginResponse);
     }
 
--- back-end/src/main/java/org/ssafy/bibibig/oauth/service/SocialService.java
+++ back-end/src/main/java/org/ssafy/bibibig/oauth/service/SocialService.java
@@ -1,38 +1,30 @@
 package org.ssafy.bibibig.oauth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.ssafy.bibibig.common.dto.ErrorCode;
+import org.ssafy.bibibig.common.exception.CommonException;
 import org.ssafy.bibibig.oauth.domain.Member;
 import org.ssafy.bibibig.oauth.dto.MemberInfo;
-import org.ssafy.bibibig.oauth.dto.response.LoginResponse;
 import org.ssafy.bibibig.oauth.repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
 public class SocialService {
-    private final MemberRepository memberRepository;
-    private HttpStatus httpStatus;
 
+    private final MemberRepository memberRepository;
     @Transactional
     public MemberInfo checkLogin (MemberInfo memberInfo) {
         try {
             Member member = memberRepository.findByEmail(memberInfo.getEmail())
                     .orElseGet(() -> memberRepository.save(memberInfo.toEntity()));
 
-             return MemberInfo.from(member);
+            return MemberInfo.from(member);
 
-        } catch (DataAccessException e) {
-            //todo 예외처리
-            System.out.println("데이터 저장 시 예외");
         } catch (Exception e) {
-            //todo 예외처리
-            System.out.println("예외 발생");
+            throw new CommonException(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
         }
-        // todo 삭제 후 예외처리로 반환
-        return null;
     }
 
 }
