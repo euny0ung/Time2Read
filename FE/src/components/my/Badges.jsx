9863f4a42e48f6bfca1c180886292d5f6cfb69a1
정혜진 <pado7sea@gmail.com>
2024-03-23 18:22:08 +0900

    #41 - feat : 내가 푼 연도 뱃지 api 구현, mock api 테스트 완료
    api를 호출을 마이페이지에서 할지 Badges컴포넌트에서 할지 정하지 못했음. 물어볼 것

--- FE/src/components/my/Badges.jsx
+++ FE/src/components/my/Badges.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react';
 import BadgeComponent from './BadgeComponent.jsx';
+import getBadge from '../../apis/myApi.jsx';
 
+// 유저가 가지고 있는 뱃지 불러와서 보여줄 거임
+// 여기서 api 호출할지 마이페이지에서 호출할지 아직 못 정함
 const Badges = () => {
+  const [badges, setbadges] = useState([]);
+
+  useEffect(() => {
+    getBadge()
+      .then((data) => {
+        setbadges(data.badges);
+        console.log('Badge Data:', data.badges);
+      })
+      .catch((error) => {
+        console.error('Error requesting badge:', error);
+      });
+  }, []);
+
   return (
     <>
-      <div />
-      <div className="space-x-4">
-        <BadgeComponent year={2021} successCount={3} /> {/* 금메달 뱃지 */}
-        <BadgeComponent year={2022} successCount={2} /> {/* 은메달 뱃지 */}
-        <BadgeComponent year={2023} successCount={1} /> {/* 동메달 뱃지 */}
+      <div>
+        <div className="space-x-4">
+          {badges.map((badge) => (
+            <BadgeComponent key={badge.year} year={badge.year} successCount={badge.count} />
+          ))}
+        </div>
       </div>
     </>
   );
