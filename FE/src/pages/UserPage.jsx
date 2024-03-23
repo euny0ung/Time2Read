cf1eda79affbc7699db2e1d612190e1b4b6e07e9
정혜진 <pado7sea@gmail.com>
2024-03-23 17:41:40 +0900

    #41 - feat : 뱃지 디자인 홀로그램 적용, 회전 효과 적용

--- FE/src/pages/UserPage.jsx
+++ FE/src/pages/UserPage.jsx
@@ -1,7 +1,16 @@
+// import BadgeFrame from '../components/my/BadgeFrame.jsx';
+import YearBadge from '../components/my/YearBadge.jsx';
+
 const UserPage = () => {
   return (
     <>
       <div />
+      <div className="space-x-4">
+        <YearBadge year={2021} successCount={3} /> {/* 금메달 뱃지 */}
+        <YearBadge year={2022} successCount={2} /> {/* 은메달 뱃지 */}
+        <YearBadge year={2023} successCount={1} /> {/* 동메달 뱃지 */}
+      </div>
+      {/* <BadgeFrame /> */}
     </>
   );
 };
