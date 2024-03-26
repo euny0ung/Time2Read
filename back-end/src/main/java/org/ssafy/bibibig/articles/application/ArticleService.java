3cdc56eaffb6a6652c3d0f9338c870825c1c2573
이민우 <lmw7414@naver.com>
2024-03-26 11:14:08 +0900

    #55 - feat : [BE] 퀴즈로직 변경으로 인한 새로운 엔드포인트 추가
    기존의 엔드포인트는 테스트를 위해 남겨놓고, 새로운 엔드포인트를 추가하였다. 해당 메서드는 키워드 문제에서 초성 힌트를 추가하였다. 기존 단어 정의는 들어가지 않는다.

--- back-end/src/main/java/org/ssafy/bibibig/articles/application/ArticleService.java
+++ back-end/src/main/java/org/ssafy/bibibig/articles/application/ArticleService.java
@@ -34,11 +34,15 @@ public class ArticleService {
         return articleRepository.getTopKeywordsByYear(year);
     }
 
-    public ArticleWithQuiz getArticleWithQuiz(Article article) {
-        return ArticleWithQuiz.from(article, makeQuiz(article));
+    public ArticleWithQuiz getArticleWithQuiz(Article article, CategoryType category) {
+        return ArticleWithQuiz.from(category, article, makeQuiz(article));
     }
 
-    public List<ArticleWithQuiz> getArticleWithQuizes(int year) {
+    public ArticleWithQuiz getArticleWithQuiz(Article article, CategoryType category, String keyword) {
+        return ArticleWithQuiz.from(category, article, makeQuiz(article, keyword));
+    }
+
+    public List<ArticleWithQuiz> getArticleWithQuizzes(int year) {
         List<ArticleWithQuiz> result = new ArrayList<>();
         List<CategoryType> randomCategory = randomCategory();
         List<CategoryType> categories = List.of(CategoryType.values());
@@ -48,11 +52,41 @@ public class ArticleService {
             List<KeywordTerms> keywords = getTopKeywordsByYearAndCategory(year, category, size);
             for (KeywordTerms keyword : keywords) {
                 Article article = getRandomArticleByYearAndCategoryAndKeyword(year, category, keyword.word());
-                result.add(getArticleWithQuiz(article));
+                result.add(getArticleWithQuiz(article, category, keyword.word()));
             }
         }
         return result;
     }
+
+    public List<ArticleWithQuiz> getFirstArticleWithQuizzes(int year) {
+        List<ArticleWithQuiz> result = new ArrayList<>();
+        List<CategoryType> categories = firstCategory();
+
+        for (CategoryType category : categories) {
+            System.out.println(category.getName());
+            List<KeywordTerms> keywords = getTopKeywordsByYearAndCategory(year, category, 1);
+            for (KeywordTerms keyword : keywords) {
+                Article article = getRandomArticleByYearAndCategoryAndKeyword(year, category, keyword.word());
+                result.add(getArticleWithQuiz(article, category, keyword.word()));
+            }
+        }
+        return result;
+    }
+
+    public List<ArticleWithQuiz> getSecondArticleWithQuizzes(int year) {
+        List<ArticleWithQuiz> result = new ArrayList<>();
+        List<CategoryType> categories = secondCategory();
+
+        for (CategoryType category : categories) {
+            List<KeywordTerms> keywords = getTopKeywordsByYearAndCategory(year, category, 1);
+            for (KeywordTerms keyword : keywords) {
+                Article article = getRandomArticleByYearAndCategoryAndKeyword(year, category, keyword.word());
+                result.add(getArticleWithQuiz(article, category));
+            }
+        }
+        return result;
+    }
+
 
     private List<KeywordTerms> getTopKeywordsByYearAndCategory(int year, CategoryType categoryType, int size) {
         List<KeywordTerms> result = new ArrayList<>();
@@ -74,22 +108,50 @@ public class ArticleService {
         return Article.from(articleRepository.getRandomArticleByYearAndCategoryAndKeyword(year, category, keyword));
     }
 
-    // 랜덤카테고리
+    //TODO - 사회 기사 추가되면 카테고리 리스트 수정할 것
+    //랜덤카테고리 - 10개
     private List<CategoryType> randomCategory() {
-        // 대분류 별 기사가 전부 들어있지 않은 관계로
-        return List.of(CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS, CategoryType.POLITICS);
+        return List.of(CategoryType.POLITICS, CategoryType.ECONOMY, CategoryType.CULTURE, CategoryType.INTERNATIONAL, CategoryType.SPORTS, CategoryType.INTERNATIONAL, CategoryType.POLITICS, CategoryType.ECONOMY, CategoryType.CULTURE, CategoryType.INTERNATIONAL, CategoryType.SPORTS, CategoryType.INTERNATIONAL);
+
 //        List<CategoryType> categoryList = new ArrayList<>(List.of(CategoryType.values()));
 //        Random random = new Random();
-//        for(int i = 0; i < 4; i++) {
+//        for (int i = 0; i < 4; i++) {
 //            int randomIdx = random.nextInt(CategoryType.values().length);
 //            categoryList.add(CategoryType.values()[randomIdx]);
 //        }
 //        return categoryList;
     }
 
+    // 카테고리 - 6개
+    private List<CategoryType> firstCategory() {
+        return List.of(CategoryType.POLITICS, CategoryType.ECONOMY, CategoryType.CULTURE, CategoryType.INTERNATIONAL, CategoryType.SPORTS, CategoryType.INTERNATIONAL);
+//        List<CategoryType> categoryList = new ArrayList<>();
+//        Random random = new Random();
+//        for (int i = 0; i < 6; i++) {
+//            int randomIdx = random.nextInt(CategoryType.values().length);
+//            categoryList.add(CategoryType.values()[randomIdx]);
+//        }
+//        return categoryList;
+    }
+
+    // 랜덤 카테고리 - 4개
+    private List<CategoryType> secondCategory() {
+        List<CategoryType> categoryList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            int randomIdx = random.nextInt(CategoryType.values().length);
+            categoryList.add(CategoryType.values()[randomIdx]);
+        }
+        return categoryList;
+    }
+
 
     private Quiz makeQuiz(Article article) {
         return quizUtils.makeQuiz(article);
+    }
+
+    private Quiz makeQuiz(Article article, String keyword) {
+        return quizUtils.makeQuiz(article, keyword);
     }
 
     private Article getArticleEntityOrThrowException(String id) {
