c5de147fdbc2805470774dc510d55d20dab0252c
shinyeji28 <xntm10@naver.com>
2024-03-28 12:08:46 +0900

    #75 - fix : [BE] cors method 수정

--- back-end/src/main/java/org/ssafy/bibibig/config/AppConfig.java
+++ back-end/src/main/java/org/ssafy/bibibig/config/AppConfig.java
@@ -18,7 +18,7 @@ public class AppConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://bibibig.ddnsking.com","http://j10b307.p.ssafy.io")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS","PATCH")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
