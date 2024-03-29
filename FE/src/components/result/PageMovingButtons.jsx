8c1d13f89fc33657bf7cc6f9350264e7b7509113
정혜진 <pado7sea@gmail.com>
2024-03-29 23:31:41 +0900

    #90 - feat : 플로팅 버튼 수정, 파비콘 추가

--- FE/src/components/result/PageMovingButtons.jsx
+++ FE/src/components/result/PageMovingButtons.jsx
@@ -1,18 +1,16 @@
-import ResultButton from '../commons/buttons/ResultButton.jsx';
-
 const PageMovingButton = ({ navigateToLandingPage, navigateToMyPage }) => {
   return (
     <>
       <div className="fixed z-10 flex flex-col gap-6 right-5 top-5">
         <button onClick={navigateToLandingPage}>
-          <ResultButton>
+          <div className="flex items-center justify-center px-6 py-3 text-gray-900 rounded-full shadow-xl bg-primary-yellow-0 hover:bg-opacity-80 hover:text-gray-600 focus:bg-opacity-80">
             <div className="text-lg text-center">다시 시계토끼 쫓아가기</div>
-          </ResultButton>
+          </div>
         </button>
         <button onClick={navigateToMyPage}>
-          <ResultButton>
+          <div className="flex items-center justify-center px-6 py-3 text-gray-900 rounded-full shadow-xl bg-primary-yellow hover:bg-opacity-80 hover:text-gray-600 focus:bg-opacity-80">
             <div className="text-lg text-center">내 정보 더 자세하게 보기</div>
-          </ResultButton>
+          </div>
         </button>
       </div>
     </>
